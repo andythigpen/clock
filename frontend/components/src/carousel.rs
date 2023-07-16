386bf3c39f79a64a14687123da407fb5071ee5a9
Andrew Thigpen <andyt05@gmail.com>
2023-07-16 14:52:54 -0400

    Add transitions between carousel items.

--- frontend/components/src/carousel.rs
+++ frontend/components/src/carousel.rs
@@ -1,7 +1,7 @@
+use gloo_timers::callback::Timeout;
 use js_sys::Array;
-use log::info;
 use wasm_bindgen::prelude::*;
-use web_sys::window;
+use web_sys::{window, Element};
 use yew::prelude::*;
 
 #[derive(PartialEq, Properties)]
@@ -13,16 +13,33 @@ pub struct Props {
 pub fn carousel(props: &Props) -> Html {
     let child = use_state(|| props.children.iter().next());
     let children_iter = use_mut_ref(|| props.children.clone().into_iter().cycle());
+    let node = use_node_ref();
+    let classes = classes!("flex", "flex-1", "transition", "duration-1000");
 
     use_effect_with_deps(
         {
             let child = child.clone();
             let children_iter = children_iter.clone();
+            let node = node.clone();
+            let classes = classes.clone();
             move |_| {
-                let win = window().unwrap();
                 let callback = Closure::<dyn Fn()>::wrap(Box::new(move || {
-                    child.set((*children_iter).borrow_mut().next());
+                    if let Some(elem) = node.cast::<Element>() {
+                        let mut hidden_classes = classes.clone();
+                        hidden_classes.push("opacity-0");
+                        elem.set_class_name(&hidden_classes.to_string());
+
+                        let classes = classes.clone();
+                        let children_iter = children_iter.clone();
+                        let child = child.clone();
+                        let timeout = Timeout::new(1000, move || {
+                            child.set((*children_iter).borrow_mut().next());
+                            elem.set_class_name(&classes.to_string());
+                        });
+                        timeout.forget();
+                    }
                 }));
+                let win = window().unwrap();
                 let _ = win.set_interval_with_callback_and_timeout_and_arguments(
                     callback.as_ref().dyn_ref().unwrap(),
                     5000, // TODO: configurable
@@ -35,7 +52,7 @@ pub fn carousel(props: &Props) -> Html {
     );
 
     html! {
-        <div class={classes!("flex", "flex-1", "transition")}>
+        <div ref={node} class={classes}>
             {(*child).clone()}
         </div>
     }
