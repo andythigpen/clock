838322f5fff2dcb3c87bdffb56469a5f93673ad2
Andrew Thigpen <andyt05@gmail.com>
2023-07-16 13:21:38 -0400

    Implement clock functionality.

--- frontend/components/src/clock.rs
+++ frontend/components/src/clock.rs
@@ -1,14 +1,87 @@
+use js_sys::{Array, Date};
+use log::info;
+use wasm_bindgen::prelude::*;
+use web_sys::window;
 use yew::prelude::*;
+
+fn short_day(day: u32) -> &'static str {
+    match day {
+        0 => "Sun",
+        1 => "Mon",
+        2 => "Tue",
+        3 => "Wed",
+        4 => "Thu",
+        5 => "Fri",
+        6 => "Sat",
+        _ => panic!("invalid day {day}"),
+    }
+}
+
+fn short_month(month: u32) -> &'static str {
+    match month {
+        0 => "Jan",
+        1 => "Feb",
+        2 => "Mar",
+        3 => "Apr",
+        4 => "May",
+        5 => "Jun",
+        6 => "Jul",
+        7 => "Aug",
+        8 => "Sep",
+        9 => "Oct",
+        10 => "Nov",
+        11 => "Dec",
+        _ => panic!("invalid month {month}"),
+    }
+}
+
+fn formatted_date(date: &Date) -> String {
+    let day = short_day(date.get_day());
+    let month = short_month(date.get_month());
+    let date = date.get_date();
+    format!("{day} {month} {date}")
+}
+
+fn formatted_time(date: &Date) -> String {
+    let hour = match date.get_hours() % 12 {
+        0 => 12,
+        h => h,
+    };
+    let minutes = date.get_minutes();
+    format!("{hour:02}:{minutes:02}")
+}
 
 #[function_component(Clock)]
 pub fn clock() -> Html {
+    let now = use_state(|| Date::new_0());
+
+    use_effect_with_deps(
+        {
+            let now = now.clone();
+            move |_| {
+                let win = window().unwrap();
+                let callback = Closure::<dyn Fn()>::wrap(Box::new(move || {
+                    info!("callback");
+                    now.set(Date::new_0());
+                }));
+                let _ = win.set_interval_with_callback_and_timeout_and_arguments(
+                    callback.as_ref().dyn_ref().unwrap(),
+                    1000,
+                    &Array::new(),
+                );
+                || drop(callback)
+            }
+        },
+        (),
+    );
+
     html! {
         <div class="flex flex-col">
             <div class="text-8xl">
-                {"Sat Jul 15"}
+                {formatted_date(&*now)}
             </div>
             <div class="text-[20rem] leading-[20rem] font-dosis">
-                {"05:16"}
+                {formatted_time(&*now)}
             </div>
         </div>
     }
--- frontend/pages/src/home.rs
+++ frontend/pages/src/home.rs
@@ -6,12 +6,14 @@ pub fn home() -> Html {
     html! {
         <div class="bg-gradient-45 from-darkbg-900 via-darkbg-800 to-darkbg-900 flex items-center justify-center h-screen w-screen animate-gradient bg-[size:400%_400%] p-8 gap-16">
             <Clock />
-            // <WeatherCurrent />
+            <div class="flex-1 flex transition">
+                <WeatherCurrent />
+            </div>
             // <WeatherForecast />
             // <WeatherHumidity />
             // <TaskReminder />
             // <CalendarReminder />
-            <Alert />
+            // <Alert />
         </div>
     }
 }
